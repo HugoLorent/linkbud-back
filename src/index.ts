f843961b3b7ad2724b813ad6b892b431ee54a0a1
HugoLorent <hugo.lorent@outlook.com>
2022-11-15 19:45:34 +0100

    add node-postgres dependency
    Signed-off-by: HugoLorent <hugo.lorent@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 const port = 5200;
 
 app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello Linkbud');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
